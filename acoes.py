dd3142f2dcdde107de23de5d8303e39c56b2ae5a
lfvelosoh <luis-1270@hotmail.com>
2023-05-11 09:46:41 -0300

    incluindo override

--- acoes.py
+++ acoes.py
@@ -3,3 +3,10 @@ import pandas as pd
 from pandas_datareader import data as pdr
 import yfinance as yf
 import datetime
+
+#realizando override para trabalhar com o pandas_datareader
+yf.pdr_override()
+
+#definindo data de inicio e fim, caso necessario
+start = '2020-01-01'
+end = '2023-03-01'
